ea038db881e2e88a217dafeb2353f0580b1ecddf
Seth <dozingducklet@gmail.com>
2017-09-11 18:49:54 +0100

    Devide instead of add

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ int0 = int(input("What is your first number."));
 int1 = int(input("What is your second number."));
 int2 = int(input("What is your third number."));
 
-number = (int0 * int1) + int2;
+number = (int0 * int1) / int2;
 print(number);
