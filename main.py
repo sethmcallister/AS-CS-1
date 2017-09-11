e6506d1d89c7f0b44690dff6b5d8ae8efe303da9
Seth <dozingducklet@gmail.com>
2017-09-11 18:49:16 +0100

    Inital commit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 int0 = int(input("What is your first number."));
 int1 = int(input("What is your second number."));
 int2 = int(input("What is your third number."));
-int3 = int(input("What is your forth number."));
 
-number = int0 + int1 + int2 + int3;
+number = (int0 * int1) + int2;
 print(number);
