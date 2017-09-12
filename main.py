66bafa6d311e1ae4d4d9d986ef034258ac359bc7
Seth <dozingducklet@gmail.com>
2017-09-12 09:20:37 +0100

    Exchange rates

--- main.py
+++ main.py
@@ -1,21 +1,5 @@
-class Lift(object):
-    height = 10;
-    width = 10;
-    depth = 10;
+EXCHANGE_RATE = 1.10299;
 
-    def __init__(self, height, width, depth):
-        self.height = height;
-        self.width = width;
-        self.depth = depth;
-
-
-class Fridge(object):
-    height = 5;
-    width = 5;
-    depth = 5;
-
-    def __init__(self, height, width, depth):
-        self.height = height;
-        self.width = width;
-        self.depth = depth;
-
+theyHave = int(input("How much money would You like to convert?"));
+output = theyHave * EXCHANGE_RATE;
+print("You will have " + str(output) + " euros.");
